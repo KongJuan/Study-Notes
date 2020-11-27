83c54eebf62308a46f0b98372c60d2c57123348b
KongJuan <kongjuan_k@163.com>
2020-11-27 09:57:01 +0800

    SpringMvc

--- 10-TestCode/springmvc-test/src/main/java/com/kong/controller/HelloController.java
+++ 10-TestCode/springmvc-test/src/main/java/com/kong/controller/HelloController.java
@@ -4,6 +4,9 @@ package com.kong.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
 
 @Controller
 public class HelloController {
@@ -12,5 +15,13 @@ public class HelloController {
     @ResponseBody
     public String showData(){
         return "showData";
+    }
+
+    @RequestMapping("/modelandview")
+    public ModelAndView result(){
+        ModelAndView mv=new ModelAndView();
+        mv.addObject("time",new Date());
+        mv.getModel().put("name","haha");
+        return mv;
     }
 }
